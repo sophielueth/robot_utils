554f5b3e2e52a0a276eef1ab9ab5ae47415ecc3b
sophielueth <sophie.lueth@me.com>
2024-06-16 22:59:10 +0200

    repo setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='robot_utils',
+      version='0.0',
+      description='Utilities for Robotics',
+      author='Sophie Lueth',
+      author_email='sophie.lueth@me.com',
+      license='MIT',
+      packages=['robot_utils'],
+      #install_requires=['numpy==1.21', ] # TBD
+      )
